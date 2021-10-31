73bc9140d54e69d55ba297161d9239cd1ff9fa64
grim <vladdewitt@gmail.com>
2021-10-31 02:42:11 -0700

    kvadrat

---
+++ src/main/java/VertexBufferSystem.java
@@ -0,0 +1,26 @@
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
+
+
+
+// create vertex array
+
+public class VertexBufferSystem {
+    int[] elements;
+    float[] vertices;
+
+    FloatBuffer vertexFB;
+
+    IntBuffer elementB;
+    //vbo
+    IntBuffer buffers;
+    //vao
+    IntBuffer vertexArray;
+    //ea
+    IntBuffer elementArray;
+
+    public VertexBufferSystem() {
+
+    }
+
+}
